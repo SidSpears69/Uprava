2c4d100c5b00626b9449ace29ac500dd2eb8e23c
SidSpears <sidspears55@gmail.com>
2020-03-10 22:26:29 +0300

    desktop adaptive

--- source/js/script.js
+++ source/js/script.js
@@ -1,22 +1,25 @@
-$(document).ready(function(){
-    $(".button--menu").click(function(){
+$(document).ready(function () {
+    $(".button--menu").click(function () {
         $(".menu").toggleClass("opened");
+        $("body").toggleClass("scroll-disabled");
     });
-    $(".button--payment").each(function(){
-        $(this).click(function(){
+    $(".button--payment").each(function () {
+        $(this).click(function () {
             $(".popup").addClass("opened");
             var top = $(this).offset().top;
             var width = $(".popup__content").outerWidth();
             var widthButton = $(this).outerWidth();
             var left = $(this).offset().left - width + widthButton;
             var difference = top + $(".popup__content").outerHeight() + $("footer").outerHeight() - $(document).height();
-            if(difference > 0) {
+            if (difference > 0) {
                 top = top - difference;
             }
-            $(".popup__content").offset({top:top, left:left});
+            $(".popup__content").offset({ top: top, left: left });
         })
     })
-    $(".button--close").click(function(){
-            $(".popup").removeClass("opened");
-        })
+    $(".button--close").click(function () {
+        $(".popup").removeClass("opened");
+    })
+    $(".menu").theiaStickySidebar({
+    });
 })

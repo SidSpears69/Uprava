d931a8924a3926ef6126857150e0b9fdadfb3ba4
SidSpears <sidspears55@gmail.com>
2020-03-17 13:19:08 +0300

    fix popup

--- source/js/script.js
+++ source/js/script.js
@@ -15,16 +15,22 @@ $(document).ready(function () {
             var widthButton = $(this).outerWidth();
             var left = $(this).offset().left - width + widthButton;
             var difference = top + $(".popup__content").outerHeight() + $("footer").outerHeight() - window.innerHeight;
+            var margin = parseInt($('.page-main').css('marginTop'));
             if (difference > 0) {
                 top = top - difference;
             }
             if(left < 0) {
                 left = window.innerWidth/2 - width/2;
             }
+            if(top < margin) {
+                $(".popup").addClass("fixed");
+            }
             $(".popup__content").offset({ top: top, left: left });
+            $("body").toggleClass("scroll-disabled");
         })
     })
     $(".button--close").click(function () {
         $(".popup").removeClass("opened");
+        $("body").toggleClass("scroll-disabled");
     })
 })
